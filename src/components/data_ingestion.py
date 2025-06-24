ee56fa5067999384be28f0c2ddb86d91eeb04247
taga1234 <tagirzhakupov1213@gmail.com>
2025-06-24 17:02:01 +0600

    Model trainer done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,9 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 
 @dataclass
 class DataIngestionConfig:
@@ -44,7 +47,7 @@ class DataIngestion:
             test_set.to_csv(self.ingestion_config.test_data_path,
                             index=False, header=True)
 
-            logging.info("Inmgestion of the data is completed")
+            logging.info("Ingestion of the data is completed")
 
             return (
                 self.ingestion_config.train_data_path,
@@ -62,3 +65,6 @@ if __name__ == "__main__":
     data_transformation = DataTransformation()
     train_arr, test_arr, _ = data_transformation.initiate_data_transformation(
         train_data, test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
